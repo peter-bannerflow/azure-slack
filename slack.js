623c21bcafb69659aedbb5cfaf667066df72625f
peter-bannerflow <peter.bergman@bannerflow.com>
2016-04-22 16:03:25 +0200

    style: Remove comment

--- slack.js
+++ slack.js
@@ -26,7 +26,6 @@ rtm.start();
 
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
-  // This will send the message 'this is a test message' to the channel identified by id 'C0CHZA86Q'
   rtm.sendMessage(message, room, function messageSent(err, res) {
     if (err == null) {
           console.log('Successfully notified the "' + room + '" hipchat room.');
